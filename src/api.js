0e0b5f2ea7ca97920d85ae809d8badd1e3e18af4
neha-rapolu001 <149213367+neha-rapolu001@users.noreply.github.com>
2024-10-07 03:16:20 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000/';
+const BASE_URL = 'ec2-18-217-84-99.us-east-2.compute.amazonaws.com';
 
 const api = axios.create({
     baseURL: BASE_URL,
